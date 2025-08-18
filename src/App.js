6d3d207a4cfb6db3e9ef51a57a36dba3df6c74de
Yonna <yonnaklaassen@gmail.com>
2025-08-18 16:13:13 +0200

    Add CV

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   const bgRef = useRef(null);
   const h3TextRef = useRef(null);
   const h1Ref = useRef(null);
+  const imgref = useRef(null);
+  const buttonRef = useRef(null);
 
   useEffect(() => {
     const colors = [
@@ -31,13 +33,18 @@ function App() {
     gsap.set(bgRef.current, { background: colors[0] });
     animateGradient();
 
-    gsap.fromTo(h1Ref.current, { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 1 });
+    gsap.fromTo(h1Ref.current, { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 0.9 });
+
+    gsap.fromTo(imgref.current, {opacity: 0, y: 20}, {opacity: 1, y: 0, duration: 2.5, delay: 2});
+    gsap.fromTo(imgref.current, {xPercent: 50}, {xPercent: -40, duration: 2, delay: 4});
+
+    gsap.fromTo(buttonRef.current, { opacity: 0}, { opacity: 1, duration: 0.9, delay: 5.5 });
 
     const text = "Software Engineer";
     const h3Text = h3TextRef.current;
     h3Text.textContent = '';
 
-    const tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
+    const tl = gsap.timeline({ repeat: -1, repeatDelay: 1.5 });
 
     for (let i = 0; i <= text.length; i++) {
       tl.to(h3Text, {
@@ -67,12 +74,18 @@ function App() {
   }, []);
 
   return (
-    <div className="App" ref={bgRef}>
+    <div className='App' ref={bgRef}>
       <h1 ref={h1Ref}>Yonna Klaassen</h1>
       <h3>
         <span ref={h3TextRef}></span>
-        <span className="cursor">|</span>
+        <span className='cursor'>|</span>
       </h3>
+      <div className='container'>
+      <img ref={imgref} id='pfp' src='me.jpg' alt='Yonna Klaassen'></img>
+      <form method='get' action={'Yonna_Klaassen_CV.pdf'}>
+          <button type='submit' ref={buttonRef} className='button'>View CV</button>
+      </form>
+      </div>
     </div>
   );
 }
